2afa4e432725563f4972863b7020c0fdf358f625
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-01-20 00:29:23 -0200

    atualizando a descriçao do desafio

--- easy-revisinig-the-select-query-I.sql
+++ easy-revisinig-the-select-query-I.sql
@@ -1,3 +1,6 @@
+--- Challenge: Query all columns for all American cities in CITY with populations larger than 100000. 
+--- The CountryCode for America is USA.
+
 SELECT *
 FROM CITY
 WHERE COUNTRYCODE = 'USA'
